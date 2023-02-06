c451dd180e1ecf05ff973c4c7523ef3150400feb
Ian <ianhoefsloot@hotmail.com>
2023-02-06 13:56:13 +0100

    gg

--- agenda/agenda/blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
-
-class Post(models.Model):
-   costs = models.CharField(max_length=10)
-   description = models.TextField()
-   date = models.DateTimeField()
-   start_end = models.DateTimeField()
