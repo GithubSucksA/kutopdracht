26cfcecf40fe9d948390ddc302e2ef92694096ea
Ian <ianhoefsloot@hotmail.com>
2023-02-06 20:14:24 +0100

    ss

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#btn').on('click', function(){
+        $.ajax({
+            url: 'https://githubsucksa.github.io/kutopdracht/blogdata.html'
+        })
+    })
+});
