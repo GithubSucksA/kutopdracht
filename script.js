c559a32b41a8a499dcd2a795c61ed6074e1c4e1d
Ian <ianhoefsloot@hotmail.com>
2023-02-12 09:44:01 +0100

    aa

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
     $('#btn').on('click', function(){
         $.ajax({
-            url: 'https://githubsucksa.github.io/kutopdracht/blogdata.html'
+            url: 'blogdata.html',
+            type: 'GET',
+            success: function (data) {
+                console.log(data);
+                var y = JSON.parse(data);
+                let entries = Object.entries(y);
+                console.log(entries);
+                for (let i = 0; i <= 2; i++) {
+                    let mult = entries.length;
+                    let x = Math.floor(Math.random() * (mult));
+                    $('.link' + (i + 1)).html('<h3>' + entries[x][0] + '</h3>');
+                    $('.tab' + (i + 1)).html('<p>' + entries[x][1] + '</p>');
+                    entries.splice(x, 1);
+                }
+            },
+            // Error handling 
+            error: function (error) {
+                console.log(`Error ${error}`);
+            }
         })
+    })
+
+    $('body').on('click', 'h3', function(e){
+        console.log(e);
     })
 });
